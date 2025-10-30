2a1aea0f398b8fbd54d2f400551fb33d2ed5d201
Alex <89070284+Alexander14758@users.noreply.github.com>
2025-10-30 19:33:44 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the airdrop end date
-const endDate = new Date("2025-10-30T15:20:00").getTime(); // Change this date
+const endDate = new Date("2025-11-01T15:20:00").getTime(); // Change this date
 
 function updateTimer() {
   const now = new Date().getTime();
